f62a828ae799b1c7a2d3866288647c9747b910ce
yulvjun <lvjun.yu@yulvjundeMacBook-Pro.local>
2020-11-07 17:53:41 +0800

    [ylj]: finish problem3-3

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,13 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
-
     private Foo foo;
 
-    public Bar(Foo foo) {
+    public void setFoo(Foo foo) {
         this.foo = foo;
     }
 
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,16 +1,24 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Foo {
-
+    @Autowired
     private Bar bar;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
+    @PostConstruct
+    public void init() {
+        bar.setFoo(this);
     }
 
+    public Bar getBar() {
+        return bar;
+    }
+    
     public void hi() {
         System.out.println("Hi, " + bar.name());
     }
