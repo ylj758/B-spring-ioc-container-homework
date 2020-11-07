d9a0ced2bd2a5b35ab8eb4441713767ed5458562
yulvjun <lvjun.yu@yulvjundeMacBook-Pro.local>
2020-11-07 17:10:14 +0800

    [ylj]: implement problem2-1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,17 +1,23 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    private  GreetingService greetingService;
+    private  ApplicationContext context;
 
-    private final GreetingService greetingService;
-
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingController(ApplicationContext context) {
+        this.context = context;
+        this.greetingService = this.context.getBean("greetingService",GreetingService.class);
+        System.out.println("greetingService build");
     }
 
     @GetMapping("/greet")
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(
+//        value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+//        proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
-
     String sayHi() {
         return "hello world";
     }
