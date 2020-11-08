1055991e7ef78ff1d12236197865afea26f240f1
yulvjun <lvjun.yu@yulvjundeMacBook-Pro.local>
2020-11-08 17:25:57 +0800

    [ylj]: finish problem2-2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,26 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
+    private GreetingService greetingService;
+    private final ApplicationContext applicationContext;
 
     @Autowired
-    public GreetingController(GreetingService greetingService) {
+    public GreetingController(GreetingService greetingService, ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
         this.greetingService = greetingService;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        greetingService = applicationContext.getBean(GreetingService.class);
+        return greetingService.sayHi()+greetingService.toString();
     }
 
 }
